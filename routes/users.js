7634c962b91062219e3e3337cc419a7e50a11b70
FANOYRIOS GKIALIS <aeroporoy10@hotmail.com>
2023-01-31 14:35:11 +0200

    some test code in user.js

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+// import { createClient } from '@supabase/supabase-js';
+
+// const supabaseUrl = 'https://uvetbyitnyfyvesznfxw.supabase.co';
+// const supabaseKey = process.env.SUPABASE_KEY;
+// const supabase = createClient(supabaseUrl, supabaseKey);
+
 /* GET sign-up page. */
 router.get('/sign-up', function (req, res, next) {
+  res.render('sign-up');
+});
+
+router.post('/sign-up', async function (req, res, next) {
+  const { data, error } = await supabase.from('User').insert([
+    {
+      emailAddress: 'abc@hotmail.com',
+      password: '12345',
+      isSuperAdmin: false,
+    },
+  ]);
   res.render('sign-up');
 });
 
